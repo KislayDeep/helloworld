d2d64b71a40c3e58ccab7e95781fe307da14389d
kislaydeep <kislay.deep@gameskraft.com>
2022-01-19 21:09:16 +0530

    Removed lambda expression

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,33 +2,33 @@ package com.company;
 
 import java.util.*;
 
-@FunctionalInterface
-interface Test {
-    public int check(int a1, int a2);
-}
+//@FunctionalInterface
+//interface Test {
+//    public int check(int a1, int a2);
+//}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Test a = (int a1,int a2) -> {
-            System.out.println("Hello " + (a1+a2));
-            return 123;
-        };
+//        Test a = (int a1,int a2) -> {
+//            System.out.println("Hello " + (a1+a2));
+//            return 123;
+//        };
+//
+//        int tmp = a.check(5,3);
+//
+//        System.out.println(tmp);
 
-        int tmp = a.check(5,3);
+        List <String> a = Arrays.asList("apple");
+        System.out.println(a);
 
-        System.out.println(tmp);
 
-//        List <String> a = Arrays.asList("apple");
-//        System.out.println(a);
-//
-//
-//
-//        Fruit f = new Fruit();
-//
-//        f.name = "Apple";
-//        f.eat();
-//        f.eat();
+
+        Fruit f = new Fruit();
+
+        f.name = "Apple";
+        f.eat();
+        f.eat();
     }
 }
